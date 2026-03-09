e6c7d9a50e2072ee5f6f72f62bc982aea2e9c044
FARHAN ABID <156011263+farhanabid786@users.noreply.github.com>
2026-03-09 12:38:10 +0530

    Update Predict.jsx

--- frontend/src/pages/Predict.jsx
+++ frontend/src/pages/Predict.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "http://agriyield-backend-c5nc.onrender.com/predict_rf";
 
 const cropTypes = [
   "Wheat",
@@ -213,3 +213,4 @@ export default function Predict() {
     </div>
   );
 }
+
